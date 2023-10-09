4f480b77653a6a421848d5fb0690d608b8ff50ac
Bjarne Johansson <bjarne.johansson@se.abb.com>
2023-10-09 17:55:55 +0200

    Started communication classes.

--- src/osa/NwComFactory.h
+++ src/osa/NwComFactory.h
@@ -1,4 +1,8 @@
+#ifndef OSFUNCTIONS_NW_COM_FACTORY_H_
+#define OSFUNCTIONS_NW_COM_FACTORY_H_
 
+#include "IReceiver.h"
+#include "ISender.h"
 #include <memory>
 class NwCComFactory
 {
@@ -9,4 +13,7 @@ public:
 		UDP_UNICAST
 	};
 	static std::unique_ptr<ISender> CreateSender(const Type type);
+	static std::unique_ptr<ISender> CreateReceiver(const Type type);
 };
+
+#endif //OSFUNCTIONS_NW_COM_FACTORY_H_
---
+++ src/osa/rcv/UDPMCastRcv.h
@@ -0,0 +1,13 @@
+#ifndef OSFUNCTIONS_UDPMCASTRCV_H_
+#define OSFUNCTIONS_UDPMCASTRCV_H_
+#include "../IReceiver.h"
+
+class CUDPMCastRcv : public IReceiver
+{
+  public:
+    
+};
+
+
+
+#endif //OSFUNCTIONS_UDPMCASTRCV_H_
